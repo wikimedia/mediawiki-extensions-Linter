958623f0fb0b7756e08ce108ef3ba5842c8f3fb3
Arlo Breault <abreault@wikimedia.org>
2016-11-15 15:25:29 -0800

    Display self-closed tags
    Change-Id: I6c084a6c72114b57a21cd367d398c673c5c4d991

--- includes/LintErrorsPager.php
+++ includes/LintErrorsPager.php
@@ -95,7 +95,7 @@ class LintErrorsPager extends TablePager {
 
 				return $this->msg( 'linker-page-title-edit' )->rawParams( $viewLink, $editLink )->escaped();
 			case 'details':
-				$hasNameCats = [ 'obsolete-tag', 'missing-end-tag' ];
+				$hasNameCats = [ 'obsolete-tag', 'missing-end-tag', 'self-closed-tag' ];
 				if ( in_array( $this->category, $hasNameCats ) && isset( $lintError->params['name'] ) ) {
 					return Html::element( 'code', [], $lintError->params['name'] );
 				} elseif ( $this->category === 'bogus-image-options' && isset( $lintError->params['items'] ) ) {
