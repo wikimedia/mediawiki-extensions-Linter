a5e2f822f375413c5a03ec37f740f7649374bd2e
Subramanya Sastry <ssastry@wikimedia.org>
2017-04-18 22:53:24 -0500

    Show tag name for the stripped-tag category
    Change-Id: I53546c11391fb2388dbcf3f70c0a21b2b1af9b09

--- includes/LintErrorsPager.php
+++ includes/LintErrorsPager.php
@@ -120,7 +120,13 @@ class LintErrorsPager extends TablePager {
 			case 'details':
 				// While deletable-table-tag has params set, it adds no new information
 				// and is not included here since the error category has the tag name.
-				$hasNameCats = [ 'obsolete-tag', 'missing-end-tag', 'self-closed-tag', 'misnested-tag' ];
+				$hasNameCats = [
+					'obsolete-tag',
+					'missing-end-tag',
+					'self-closed-tag',
+					'misnested-tag',
+					'stripped-tag',
+				];
 				if ( in_array( $this->category, $hasNameCats ) && isset( $lintError->params['name'] ) ) {
 					return Html::element( 'code', [], $lintError->params['name'] );
 				} elseif ( $this->category === 'bogus-image-options' && isset( $lintError->params['items'] ) ) {
